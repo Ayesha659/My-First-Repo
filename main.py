c43c722ec5d3e284e510033cf49ce98a006418d8
Ayesha <ayeshanadeem1980659@gmail.com>
2022-01-05 00:25:55 +0500

    3rd commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-# print("Hello World")
-x=int(input())
-if x%2==0:
-   print(x,"is an even number") 
-else:
-    print(x,"is an odd number")
+# Find even number
+def even_num(x):
+   if x%2==0:
+      print(x,"is an even number") 
+   else:
+      print(x,"is an odd number")
+   return x
+
+# Find average 
+def average(x,y):
+   z=(x/y)%2
+   return (z)
 
